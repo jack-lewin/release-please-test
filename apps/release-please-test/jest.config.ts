ef073e1ce1147428b8723af35836588872d57669
Jack Lewin <14926880+jack-lewin@users.noreply.github.com>
2024-07-06 15:02:12 +0100

    Initial commit

---
+++ apps/release-please-test/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'release-please-test',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/release-please-test',
+};
